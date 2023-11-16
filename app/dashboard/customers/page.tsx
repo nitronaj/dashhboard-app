ae102a568a1d6e596eaaa9a050d49062288f3d00
Charlie Nader <nitro.naj@gmail.com>
2023-11-16 13:52:33 +1100

    Adding Auth and metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
 const Customers = () => {
-	return <div>Customers Component</div>;
+  return <div>Customers Component</div>;
 };
 
 export default Customers;
