2f50de9dfa42faed9009d8d23ec06b256a9f77ee
Charlie Nader <nitro.naj@gmail.com>
2023-11-19 01:31:10 +1100

    update mather in middleware

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,13 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
